d15332046b8b2b188c8f4fb7dd25b3fd275b7cfc
bryan <bryanchu10@gmail.com>
2024-11-25 00:29:57 +0800

    重構應用程式結構，新增前端佈局與日曆插件，更新樣式與配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/styles/all.scss'],
+  components: {
+    dirs: [
+      {
+        path: '~/components/global',
+        global: true
+      }
+    ]
+  },
+  app: {
+    baseURL: '/',
+  }
 })
